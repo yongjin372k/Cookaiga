5bdcade4e08e03bc390e6d11d8ad6281222b5a6a
yongjin372k <leeyongjin372k@gmail.com>
2024-12-31 16:26:28 +0800

    Working Image Analysis
    Captioning Tested and Working. Need to try Dense Captioning

--- server/src/main/java/com/cookaiga/demo/DemoApplication.java
+++ server/src/main/java/com/cookaiga/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.cookaiga.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cookaiga.demo.imageanalysis.ImageAnalysis;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @Autowired
+    private ImageAnalysis imageAnalysis;
 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        imageAnalysis.performImageAnalysis();
+    }
 }
