512c8002c8a3cf9192302b88c674166f4522e95f
yongjin372k <143159303+yongjin372k@users.noreply.github.com>
2025-01-07 20:05:26 +0800

    Update cookaiga.sql

--- cookaiga.sql
+++ cookaiga.sql
@@ -5,16 +5,23 @@ create database if not exists cookaiga;
 use cookaiga;
 
 -- USER TABLE -- 
-drop table user;
 CREATE TABLE IF NOT EXISTS USER (
-	userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	username VARCHAR(255) NOT NULL UNIQUE,
-	password_hash VARCHAR(255) NOT NULL,                         -- May not need if hardcode
-	role ENUM('consumer', 'admin') NOT NULL,
-	profile_pic_url VARCHAR(500),
-	description TEXT,                                                                        -- URL to user's profile picture (cloud storage URL like Azure)
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	userID INT PRIMARY KEY auto_increment,
+    username VARCHAR(50) NOT NULL,
+    fullname VARCHAR(50),
+    points INT DEFAULT 0,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+-- drop table user;
+-- CREATE TABLE IF NOT EXISTS USER (
+-- 	userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- 	username VARCHAR(255) NOT NULL UNIQUE,
+-- 	password_hash VARCHAR(255) NOT NULL,                         -- May not need if hardcode
+-- 	role ENUM('consumer', 'admin') NOT NULL,
+-- 	profile_pic_url VARCHAR(500),
+-- 	description TEXT,                                                                        -- URL to user's profile picture (cloud storage URL like Azure)
+-- 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+-- );
 
 
 -- INGREDIENTS TABLE --
@@ -27,17 +34,25 @@ CREATE TABLE IF NOT EXISTS INGREDIENTS ( 				-- ** UNSURE IF NEED AN INGREDIENTS
 
 -- KITCHEN TABLE --
 drop table kitchen;
-CREATE TABLE IF NOT EXISTS KITCHEN (							-- This is the MyKitchen, keeps track of pantry items
-	kitchenID INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS KITCHEN (							-- This is the MyKitchen, Processes current items ready for cooking
+	kitchenID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    kitchenList TEXT,
 	userID INT NOT NULL,
-	foodID INT NOT NULL,
-	quantity DECIMAL(10,2),									-- Amount of ingredients the user has (e.g. 250g of meat)
-	unit VARCHAR(50),										-- Unit of measurement (e.g. grams, pieces)
-	expiry_date DATE,
 	added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (userID) REFERENCES user(userID),
-	FOREIGN KEY (foodID) REFERENCES ingredients(foodID)
+	FOREIGN KEY (userID) REFERENCES user(userID)
 );
+-- drop table kitchen;
+-- CREATE TABLE IF NOT EXISTS KITCHEN (							-- This is the MyKitchen, keeps track of pantry items
+-- 	kitchenID INT PRIMARY KEY AUTO_INCREMENT,
+-- 	userID INT NOT NULL,
+-- 	foodID INT NOT NULL,
+-- 	quantity DECIMAL(10,2),									-- Amount of ingredients the user has (e.g. 250g of meat)
+-- 	unit VARCHAR(50),										-- Unit of measurement (e.g. grams, pieces)
+-- 	expiry_date DATE,
+-- 	added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+-- 	FOREIGN KEY (userID) REFERENCES user(userID),
+-- 	FOREIGN KEY (foodID) REFERENCES ingredients(foodID)
+-- );
 
 -- RECIPE TABLE --											-- NOT SURE
 drop table recipe;
