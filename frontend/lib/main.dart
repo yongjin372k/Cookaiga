7268f01e7948b74245d95f877f9713beffe144db
yongjin372k <leeyongjin372k@gmail.com>
2025-01-07 17:05:23 +0800

    Camera to Database
    - Swapped Backend ImageUrl to ImageStream and BinaryData
    
    - Added simple Camera Page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/camera_screen.dart';
 import 'screens/recipe_screen.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RecipeScreen(),
+      home: CameraPage(),
     );
   }
 }
